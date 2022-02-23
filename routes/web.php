1ccbcf56eeb92da9ad0b7db7621e7047ee069a92
fuuba <mrfgamersjalor@gmail.com>
2022-02-23 15:07:26 +0700

    commit on 'master'

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\ContactController;
 
 
 Route::get('/', function () {
-    return view('Home', [
+    return view('index', [
         "title" => "Beranda"
     ]);
 });
@@ -34,3 +34,9 @@ Route::get('/gallery', function () {
     ]);
 });
 Route::resource('/contacts', ContactController::class);
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
